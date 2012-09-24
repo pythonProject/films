4399548c72c2951abf282d0db9537adb6fefb76d
unknown <Administrator@1-.(none)>
2012-09-24 23:51:04 +0300

    started to create the first form for uploading films

---
+++ films_project/films_app/forms.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from django import forms
+
+class UploadFilmsForm(forms.Form):
+	name = 	forms.CharField()
+	director = forms.CharField()
+	description = forms.CharField()
+	link = forms.CharField()
+	releaseDate = forms.DateTimeField()
+	authors = forms.CharField()
+	genre = forms.CharField()
