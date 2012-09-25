7c8ed8d14aa057e69e2eef800304bb719a488191
unknown <Administrator@1-.(none)>
2012-09-25 13:55:15 +0300

    corrected urls.py

--- films_project/films_project/urls.py
+++ films_project/films_project/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from test_app.views import *
+from films_app.views import *
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
