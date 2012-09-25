8bf0f5ffe594d67fcb18ceb10f8d209ef5329ba1
Oleh Portyanin <ollieportnin@gmail.com>
2012-09-25 21:57:03 +0300

    Before manya is installing the films application

--- films_project/films_app/views.py
+++ films_project/films_app/views.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render_to_response
+from forms import UploadFilmsForm
 
 def hello(request):
 	return render_to_response("hello.html", {"hello": "hello"})
@@ -10,3 +11,12 @@ def reqMeta(request):
 	# import ipdb; ipdb.set_trace()
 	res = request.META.items()
 	return render_to_response("meta.html", {"res": res})
+
+def UploadForm(request):
+	form = UploadFilmsForm()
+	if request.method == "POST":
+		form = UploadFilmsForm(request.POST)
+		if form.id_valid():
+			cd = form.cleaned_data
+			pass
+	return render_to_response("uploadFilm.html", {"form": form})
