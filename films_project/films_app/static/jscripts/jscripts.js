a8957a6876ad87f66607ba4c9724760da5e6f475
Oleh Portyanin <ollieportnin@gmail.com>
2012-10-06 02:33:29 +0300

    done the definition of html element by jquery

--- films_project/films_app/static/jscripts/jscripts.js
+++ films_project/films_app/static/jscripts/jscripts.js
@@ -1,3 +1,37 @@
+$(document).ready(function()
+{
+	$("#id_authors").focus(function()
+	{
+		if($("#f").css("display") == "none")
+			$("#f").css("display", "block");
+	});
+	$("#f").blur(function()
+	{
+		$("#f").css("display", "none");
+	});
+	$("#id_authors").keyup(function()
+		{
+			var str = $("#id_authors").val();
+			c = $("#f li").length;
+			var str2;
+			var i = 0;
+			for(i = 0; i < c; i++)
+			{
+				if($("#f li").eq(i).text().indexOf(str) === -1)
+				{	
+					$("#f li").eq(i).css("display", "none");
+				}
+				else
+				{
+					$("#f li").eq(i).css("display", "block");
+				}
+			}
+		});
+	$("#f li").click(function()
+	{
+		alert($(this).get());
+	});
+});
 function addGenre1()
 {
 	if($("#addGenreBlock").css("display") == "none")
@@ -11,43 +45,3 @@ function addGenre1()
 		$("#addGenreButton").val("Добавить жанр");		
 	}
 }
-$(document).ready(function()
-{
-	$("#id_authors").focus(function()
-	{
-		if($("#authors_list").css("display") == "none")
-			$("#authors_list").css("display", "block");
-		if($("#authors_list li").text() == "")
-		{
-			var c = $("#f li").length;
-			var v;
-			for( var i = 0; i < c; i++)
-			{
-				v = $("#f li").eq(i).text();
-				$("#authors_list").append("<li>" + v + "</li>");
-			}
-		}
-	});
-	$("#id_authors").blur(function()
-	{
-		$("#authors_list").css("display", "none");
-	});
-	$("#id_authors").keyup(function()
-		{
-			var str = $("#id_authors").val();
-			c = $("#authors_list li").length;
-			var str2;
-			var i = 0;
-			for(i = 0; i < c; i++)
-			{
-				if($("#authors_list li").eq(i).text().indexOf(str) === -1)
-				{	
-					$("#authors_list li").eq(i).css("display", "none");
-				}
-				else
-				{
-					$("#authors_list li").eq(i).css("display", "block");
-				}
-			}
-		});
-});
