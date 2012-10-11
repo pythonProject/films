e1165eabb88b96dbedfba4424ea381a59adcdb75
Oleh Portyanin <ollieportnin@gmail.com>
2012-10-11 21:42:36 +0300

    fixed regressions after the last commit

--- films_project/films_app/static/jscripts/jscripts.js
+++ films_project/films_app/static/jscripts/jscripts.js
@@ -6,75 +6,78 @@ $(document).ready(function()
         if($("#f").css("display") == "none")
             $("#f").css("display", "block");
     });
-    $("#f li").children("li").blur(function()
+    $("#f").children("li").blur(function()
     {
         $("#f").css("display", "none");
-//        for(var i = 0; i < $("#f li").length; i++)
-//        {
-//            $("#f li").eq(i).css("display", "none");
-//        }
-    });
-    $("#id_add_authors").keydown(function(e)
-    {
-        $("#added_author").css("display", "none");
-        if(e.which === 13)
+        for(var i = 0; i < $("#f li").length; i++)
         {
-            var str = $("#id_add_authors").val().trim();
-            var c = $("#f li").length;
-            var am = 0;
-            var amount = 0;
-            for(var i = 0; i < c; i++)
-            {
-                if($("#f li").eq(i).text().trim() === str & $("#f li").eq(i).css("display") !== "none")
-                {
-                    $("#authors_list").append("<li style='list-style-type: none'><input type='checkbox' id='id_" + $("#f li").eq(i).text() + "' checked='checked' onChange='removeAuthor($(this))' /> <span id='id_" + $("#f li").eq(i).text() + "'>" + $("#f li").eq(i).text() + "</span></li>");
-                    $("#f li").eq(i).css("display", "none");
-                    $("#id_add_authors").val("");
-                    am++;
-                    break;
-                }
-            }
-            if(am == 0)
-            {
-                for(var j = 0; j < $("#authors_list li").length; j++)
-                {
-                    if(str == $("#authors_list li").eq(j).children("span").text().trim())
-                       amount++;
-                }
-                if(amount == 0)
-                {
-                    $("#authors_list").append("<li style='list-style-type: none'><input type='checkbox' id='id_" + $("#id_add_authors").val() + "' checked='checked' onChange='removeAuthor($(this))' /> <span id='id_" + $("#id_add_authors").val() + "'>" + $("#id_add_authors").val() + "</span></li>");
-                    $("#id_add_authors").val("");
-                }
-                else
-                {
-                    $("#added_author").css("display", "block");
-                }
-            }
+            $("#f li").eq(i).css("display", "none");
         }
     });
-    $("#id_add_authors").keyup(function()
+    $("#id_add_authors").keyup(function(e)
         {
-            var str = $("#id_add_authors").val();
-            c = $("#f li").length;
-            for(var i = 0; i < c; i++)
+            $("#added_author").css("display", "none");
+            if(e.which === 13)
             {
-                if($("#f li").eq(i).text().indexOf(str) === -1)
+                var str = $("#id_add_authors").val().trim();
+                var c = $("#f li").length;
+                var am = 0;
+                var amount = 0;
+                for(var i = 0; i < c; i++)
                 {
-                    $("#f li").eq(i).css("display", "none");
+                    if($("#f li").eq(i).text().trim() === str & $("#f li").eq(i).css("display") !== "none")
+                    {
+                        $("#authors_list").append("<li style='list-style-type: none'><input type='checkbox' id='id_" + $("#f li").eq(i).text() + "' checked='checked' onChange='removeAuthor($(this))' /> <span id='id_" + $("#f li").eq(i).text() + "'>" + $("#f li").eq(i).text() + "</span></li>");
+                        $("#f li").eq(i).css("display", "none");
+                        am++;
+                        break;
+                    }
                 }
-                else
+                if(am == 0)
                 {
                     for(var j = 0; j < $("#authors_list li").length; j++)
                     {
-                        if($("#authors_list li").eq(j).children("span").text() === $("#f li").eq(i).text())
+                        if(str == $("#authors_list li").eq(j).children("span").text().trim())
+                            amount++;
+                    }
+                    if(amount == 0)
+                    {
+                        $("#authors_list").append("<li style='list-style-type: none'><input type='checkbox' id='id_" + $("#id_add_authors").val() + "' checked='checked' onChange='removeAuthor($(this))' /> <span id='id_" + $("#id_add_authors").val() + "'>" + $("#id_add_authors").val() + "</span></li>");
+                    }
+                    else
+                    {
+                        $("#added_author").css("display", "block");
+                    }
+                }
+            }
+            else
+            {
+                var str = $("#id_add_authors").val();
+                var c = $("#f li").length;
+                for(var i = 0; i < c; i++)
+                {
+                    if($("#f li").eq(i).text().indexOf(str) === -1)
+                    {
+                        $("#f li").eq(i).css("display", "none");
+                    }
+                    else
+                    {
+                        if($("#authors_list li").length == 0)
                         {
-                            $("#f li").eq(i).css("display", "none");
-                            break;
+                            for(var g = 0; g < $("#f li").length; g++)
+                                $("#f li").eq(i).css("display", "block");
                         }
-                        else
+                        for(var j = 0; j < $("#authors_list li").length; j++)
                         {
-                            $("#f li").eq(i).css("display", "block");
+                            if($("#authors_list li").eq(j).children("span").text() === $("#f li").eq(i).text())
+                            {
+                                $("#f li").eq(i).css("display", "none");
+                                break;
+                            }
+                            else
+                            {
+                                $("#f li").eq(i).css("display", "block");
+                            }
                         }
                     }
                 }
